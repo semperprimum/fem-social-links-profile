f154e1dcd4cb0e49168788e17cc897ec8523f636
semperprimum <bkeem0266+github@gmail.com>
2024-01-19 17:07:06 +0600

    feat: add bars icon, code cleanup, improve accessibility

--- src/components/SocialsItem.jsx
+++ src/components/SocialsItem.jsx
@@ -1,17 +1,18 @@
 import styles from "./styles/Socials.module.scss";
+import BarsIcon from "../assets/images/bars-solid.svg?react";
 
 export function SocialsItem({ social, provided }) {
   return (
     <a
-      className={styles.link}
+      role="listitem"
+      className={styles.item}
       href={social.link}
       {...provided.draggableProps}
       {...provided.dragHandleProps}
       ref={provided.innerRef}
     >
-      <li className={styles.item}>
-        <p>{social.name}</p>
-      </li>
+      {social.name}
+      <BarsIcon aria-hidden="true" className={styles.icon} />
     </a>
   );
 }
